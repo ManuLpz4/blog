35c6f810c33d9f38831525e05368bd2f6add120a
ManuLpz4 <manulpz41@gmail.com>
2024-07-04 20:13:32 -0300

    Redirec to origin on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,16 @@
+import Link from 'next/link'
+
 import { cn } from '../utils/tailwind'
 import { ThemeToggle } from './theme-toggle'
 
 export function Header({ className }: Readonly<{ className?: string }>) {
   return (
     <header className={cn('flex justify-between', className)}>
-      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-        Manu Lopez
-      </h1>
+      <Link href="/">
+        <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Manu Lopez
+        </h1>
+      </Link>
       <ThemeToggle />
     </header>
   )
